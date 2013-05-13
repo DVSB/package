e82012ffcaaffbb8ee255659972991a81d658886
Samuel Ondrek <samuel@ondrek.com>
2013-05-14 01:37:13 +0200

    Added jQuery and some client script in Browse Table

--- views/amazon/-script.js
+++ views/amazon/-script.js
@@ -3,3 +3,30 @@
 
 	// This is not server-side Javascript, this files is loaded via standard browser
 	// Do not put here ANY server-side information!
+
+	$(document).ready(function() {
+		
+		
+		// if clicked on TR, is checkbox checked/unchecked
+		$('tr').click(function(event) {
+			if (event.target.type !== 'checkbox') {
+				$(':checkbox', this).trigger('click');
+			}
+		});
+		
+		
+		// on click on .checkall checked all checkboxes
+		$('.checkall').click(function () {
+			
+			var checkboxes = $('table').find(':checkbox');
+			var isChecked = $('.checkall').prop('checked');
+			
+			checkboxes.prop('checked', function(){
+				return isChecked;
+			});
+			
+		});
+		
+		
+		
+	});
