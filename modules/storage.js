8140a51d7516b8be472f9f93161a52fe452cb77f
Samuel Ondrek <samuel@ondrek.com>
2013-08-19 16:18:55 +0200

    Update Storage and Folders

--- modules/storage.js
+++ modules/storage.js
@@ -38,50 +38,7 @@ module.exports = function(req, res) {
 
 	}; // Unlink
 	
-
-
 	
-	
-	
-	var browseFolder = function(prefix, callback){
-								
-		s3.client.listObjects({
-			Bucket : settings.bucket,
-			Delimiter : '/',
-			Prefix : prefix ? prefix : settings.home+'/'
-		}, function(err, data){
-		
-			if (err) { res.send(err); }
-						
-			// Make Array of Hashed and Unhashed Folders
-			var hash, folders=[];
-			data.CommonPrefixes.forEach(function(element, index) {
-				folders[index] = {
-					Key : element.Prefix,
-					Hash : crypto.createHash('sha512').update(element.Prefix + settings.hash).digest('hex').substr(0, 10)
-				}
-			});
-						
-			// Make Array of Hashed and Unhashed Files
-			var hash, files=[];
-			data.Contents.forEach(function(element, index){
-				files[index] = underscore.pick(data.Contents[index], 'Key', 'LastModified', 'Size');
-				hash = crypto.createHash('sha512').update(element.Key + settings.hash).digest('hex').substr(0, 10);
-				files[index] = underscore.extend(files[index], {'Hash' : hash});
-			});
-
-									
-			res.render(__dirname+'/../views/storage.html', {myfiles: {
-				folders : folders,
-				files : underscore.rest(files)
-			}});
-			
-			
-		}); // s3
-	
-	}; // browseFolder
-	
-		
 	var findFolderBasedOnPrefix = function(){
 		
 		var prefix = '';
@@ -122,6 +79,62 @@ module.exports = function(req, res) {
 		isRoot ? browseFolder('', 0) : getCurrentFolder(0);
 		
 	}; // findFolderBasedOnPrefix
+	
+	
+	var browseFolder = function(prefix, callback){
+								
+		s3.client.listObjects({
+			Bucket : settings.bucket,
+			Delimiter : '/',
+			Prefix : prefix ? prefix : settings.home+'/'
+		}, function(err, data){
+		
+			if (err) { res.send(err); }
+						
+			// Make Array of Hashed and Unhashed Folders
+			var hash, folders=[];
+			data.CommonPrefixes.forEach(function(element, index) {
+				folders[index] = {
+					Key : element.Prefix,
+					Hash : crypto.createHash('sha512').update(element.Prefix + settings.hash).digest('hex').substr(0, 10)
+				}
+			});
+						
+			// Make Array of Hashed and Unhashed Files
+			var hash, files=[];
+			data.Contents.forEach(function(element, index){
+				files[index] = underscore.pick(data.Contents[index], 'Key', 'LastModified', 'Size');
+				hash = crypto.createHash('sha512').update(element.Key + settings.hash).digest('hex').substr(0, 10);
+				files[index] = underscore.extend(files[index], {'Hash' : hash});
+			});
+			
+			// Edit date
+			files.forEach(function(ele, index){
+				ele = new Date(ele.LastModified);
+				files[index].LastModified = ele.getFullYear() + '/' + ele.getMonth() + '/' + ele.getDate() + ' ' + ele.getHours() + ':' + ele.getMinutes();
+			});
+			
+			// Edit title
+			files.forEach(function(ele, index){
+				ele = underscore.words(ele.Key, '/');
+				files[index].Key = underscore.last(ele);
+			});
+						
+			// Edit title
+			folders.forEach(function(ele, index){
+				ele = underscore.words(ele.Key, '/');
+				folders[index].Key = underscore.last(ele);
+			});
+	
+			res.render(__dirname+'/../views/storage.html', {myfiles: {
+				folders : folders,
+				files : underscore.rest(files)
+			}});
+			
+			
+		}); // s3
+	
+	}; // browseFolder
 
 
 // routing and variables
@@ -138,7 +151,7 @@ module.exports = function(req, res) {
 			break;
 		
 		case 'download' :
-			res.send('download');
+			findFolderBasedOnPrefix();
 			break;
 			
 		case 'cut' :
