ab7c9fe965568a4446a2cdb5db6f6c9ee024b78c
Samuel Ondrek <samuel@ondrek.com>
2013-11-13 23:00:36 +0100

    Tags and Synch

--- privates/tags/rename.js
+++ privates/tags/rename.js
@@ -1,6 +1,47 @@
 module.exports = function(req, res) {
 
-	
+
+	var getAllTags = function(){
+
+		var mdownapi = require('../../api/mdownapi')();
+		var userId = req.signedCookies.publickey;
+
+		mdownapi.getJson(userId, '/all/tags', function(data){
+			renameSpecificTag(data);
+		});
+
+	};
+
+
+	var renameSpecificTag = function(tags){
+
+		var oldTag = req.body.oldtag;
+		var newTag = req.body.newtag;
+
+		// replace old value in array
+		tags[tags.indexOf(oldTag)] = newTag;
+
+		uploadNewConfig(tags);
+
+	};
+
+
+	var uploadNewConfig = function(updatedTags){
+
+		var s3 = require('../../api/s3')();
+		var userId = req.signedCookies.publickey;
+		
+ 		s3.putObject({
+ 			Key : userId+'/all/tags',
+ 			Body : JSON.stringify(updatedTags),
+			Bucket : 'api.mdown.co'
+ 		}, function(){
+ 			res.redirect('/-/tags/');
+ 		});
+
+	};
+
+
+	getAllTags();
 	
 };
-
