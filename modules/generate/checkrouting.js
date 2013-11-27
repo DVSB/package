4af2a8472735616579e89fb8add77dad2c297990
Samuel Ondrek <samuel@ondrek.com>
2013-11-28 00:36:12 +0100

    Checking Rules

--- modules/generate/checkrouting.js
+++ modules/generate/checkrouting.js
@@ -3,36 +3,69 @@ module.exports = function(req, res, callback) {
 
 	var mdownapi = require('../../api/mdownapi')();
 	var userId = req.signedCookies.publickey;
+	var underscore = require('underscore');
+	underscore.mixin(require('underscore.string').exports());
 
 
-	var parseRules = function(rules){
-
-		var underscore = require('underscore');
-		underscore.mixin(require('underscore.string').exports());
+	var parseRules = function(object){
 
 		// separate rules string to array of rules
-		rules.forEach(function(ele){
-			ele.rule = underscore.words(ele.rule, ',')
+		// from: ==> type='blog', thunb = 'yes', public = ' yes '
+		// into: ==> ["type='blog'","thunb = 'yes'","public = ' yes '"]
+		object.forEach(function(ele){
+			ele.rule = underscore.words(ele.rule, ',');
 		});
 
-		// remove empty spaces from each tag
-		rules.forEach(function(ele, i){
+		// remove all white spaces and aphostrphs from each tag
+		// from: ==> ["type='blog'","thunb = 'yes'","public = ' yes '"]
+		// into: ==> ['type=blog', 'thunb=yes', 'public=yes']
+		object.forEach(function(ele, i){
 			ele.rule.forEach(function(string, j){
-				console.log(string);
-				rules[i]['rule'][j] = underscore.trim(string);
-				console.log(string);
+				string = string.replace(/\s+/g, '');
+				string = string.replace(/'/g, '');
+				object[i]['rule'][j] = string;
 			});
 		});
 
-		
+		// remove all white spaces from each tag
+		// from: ==> ['type=blog', 'thunb=yes', 'public=yes']
+		// into: ==> [[type, blog], [thunb, yes], [public, yes]]
+		object.forEach(function(ele, i){
+			ele.rule.forEach(function(string, j){
+				string = underscore.words(string, '=');
+				object[i]['rule'][j] = [ 
+					string['0'], string['1'] 
+				];
+			});
+		});
 
-		callback(rules);
+		return object;
 
-	}
+	};
+
+
+	var parsePath = function(object){
+
+		// sample data: 
+		// "path": "/blog/{{%cool-url}}.html"
+
+		// remove all white spaces
+		object.forEach(function(ele){
+			ele.path = ele.path.replace(/\s+/g, '');
+		});
+
+		// search for tags
+
+
+		return object;
+
+	};
 
 
 	mdownapi.getJson(userId, '/routing', function(rules){
-		parseRules(rules);
+		rules = parseRules(rules);
+		rules = parsePath(rules);
+		callback(rules);
 	});
 
 
