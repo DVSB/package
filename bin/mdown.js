1eaae0e8faf1c79784ceae6235f211b49433bb15
Samuel Ondrek <samuel@ondrek.com>
2014-01-14 23:45:40 +0100

    Whole new functionality of watching

--- bin/mdown.js
+++ bin/mdown.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
-"use strict";
 
-var path = require('path');
-var lib = path.join(path.dirname(require('fs').realpathSync(__filename)), '../lib');
-require(lib + '/_mdown.js')();
+    "use strict";
+
+    var path = require('path');
+    var lib = path.join(path.dirname(require('fs').realpathSync(__filename)), '../lib');
+
+    require(lib + '/_mdown.js')();
