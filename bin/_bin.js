49af84a53bd710108efb35c95fae77242059b0f9
Samuel Ondrek <samuel@ondrek.com>
2014-02-28 01:58:16 +0100

    Watch => Preview command

--- bin/_bin.js
+++ bin/_bin.js
@@ -81,7 +81,7 @@
     // second CMD argument set by user
     switch (process.argv[2]) {
 
-        case "watch" :
+        case "preview" :
             require("./echo")("welcome in mdown 0.0.20 ~ watching "+process.env.PWD);
             require("./echo")("build (localhost:3008) and dashboard (localhost:3009)".bold);
             _watchLocalhost();
@@ -99,7 +99,7 @@
 
         default :
             require("./echo")("you can use only these subcommands: ", true);
-            require("./echo")("       create, watch, deploy, report");
+            require("./echo")("       preview");
             break;
 
     }
